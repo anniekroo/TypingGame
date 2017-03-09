60ad0ac59bfa1d109971e1bf7d5226c37d3c94a6
rowansharman <rowansharman1@gmail.com>
2017-03-09 14:26:03 -0500

    more minor adjustments

--- unicodeGen.py
+++ unicodeGen.py
@@ -14,15 +14,15 @@ def get_all_unicode():
     # Update this to include code point ranges to be sampled
     include_ranges = [  # these ranges selected for interesting non-alphanumeric characters with minimal gaps
 
-        (0x2200, 0x22FF), # Mathematical Operators
-        (0x0400, 0x04FF), # Cyrillic
-        (0x0250, 0x02AF), # IPA Extensions
-        (0x30A0, 0x30FF), # Katakana, almost no missing characters
-        (0x0600, 0x06FF), # Arabic
-        (0x03A3, 0x03FF), # Greek
-        (0x05D0, 0x05EA), # Hebrew, no missing characters
-        (0x0904, 0x0939), # Devanagari
-        (0xF900, 0xFA6D), # CJK compatibility ideographs, almost no missing characters
+        (0x2200, 0x22FF),  # Mathematical Operators
+        (0x0400, 0x04FF),  # Cyrillic
+        (0x0250, 0x02AF),  # IPA Extensions
+        (0x30A0, 0x30FF),  # Katakana, almost no missing characters
+        (0x0600, 0x06FF),  # Arabic
+        (0x03A3, 0x03FF),  # Greek
+        (0x05D0, 0x05EA),  # Hebrew, no missing characters
+        (0x0904, 0x0939),  # Devanagari
+        (0xF900, 0xFA6D),  # CJK compatibility ideographs, almost no missing characters
     ]
 
     alphabet = [  # generate all unicode in ranges
