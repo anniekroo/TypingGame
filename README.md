bee44d82b70cf8289fae04f4365f79940d33349f
rowansharman <rowansharman1@gmail.com>
2017-03-09 00:20:07 -0500

    added screenshot and more readability improvements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TypingGame
 A matrix styled typing game that is designed to help teach people how to type and have fun in the process.
+
+![example screenshot](Sample.png)
--- modely.py
+++ modely.py
@@ -1,15 +1,16 @@
 import pygame, random, unicodeGen, math
 
-
-allUnicodeChoices = unicodeGen.get_all_unicode()
-
+allUnicodeChoices = unicodeGen.get_all_unicode()  # Run this once to generate the list
 
 white = 255, 255, 255
 green = 0, 255, 0
 
 class Model:
+    '''Model keeps track of the game state, including running time and
+    score-related info'''
+
     def __init__(self, hits=0, misses=0, wrongKey=0, gameOver=False):
-        '''Defines hits misses and game over and initializes the class.
+        '''keeps track of hits misses and game state and initializes the class.
         When undefined, hits and misses equal zero and gameover is false.
         '''
         self.hits = hits
@@ -18,21 +19,14 @@ class Model:
         self.gameover = gameOver
 
     def score(self):
-        '''Defines score'''
+        '''Keeps track of score'''
         total = self.hits + self.misses + self.wrongKey
         if(total == 0):
             return 0
         return (self.hits / total) * 100
 
-    def wpm(self):
-        '''sets up a function in the class for there to be a words per minute.
-        This does not yet actually portray words per minute but instead is a
-        constant. We will change this once we have fully worked out exactly how
-        the UI is interfacing with the model'''
-        return 37
-
     def updateScore(self, event, curTime=0):
-        '''updates score statistics by modifying values in the class model'''
+        '''Updates score statistics by modifying values in the class model'''
         if event == 'h':
             self.hits += 1
         elif event == 'm':
@@ -44,34 +38,40 @@ class Model:
 
 
 class Letter:
-    '''Letter objects include the letter to be typed, its location on the screen,
-    and other similar information, and the random unicode string that follows it'''
+    '''Letter objects include the letter to be typed, its location on the
+    screen, size, and related information, and the random unicode string that
+    follows it'''
 
-    def __init__(self, font='ARIALUNI.TTF', value=None, x=None, y=None, surf=None):
+    def __init__(self,
+                 font='ARIALUNI.TTF', value=None, x=None, y=None, surf=None):
         # arguments not passed in are randomly generated
         self.font = font
         self.textFont = pygame.font.Font(self.font, 40)
         self.tailFont = pygame.font.Font(self.font, 40)
 
-        charWidth = 40  # self.textFont.size('X')[0]
+        charWidth = 40
+        # by experimentation, the widest characters appear to be about 40
+        # pixels in this font and size
         charHeight = self.textFont.size('X')[1]
+        # all characters should be the same height
 
         if(value == None):
             self.value = random.randint(97, 122)
+            # this is the range a-z in ascii
         else:
             self.value = value
 
-        self.tail = []
+        self.tail = []  # tail is the random unicode string
+        # Add a random number of random characters to the tail:
         self.tailLength = random.randint(3, 12)
         for i in range(self.tailLength):
             self.tail.append(random.choice(allUnicodeChoices))
 
         self.surf = pygame.Surface((charWidth, charHeight*(self.tailLength+1)))
-        # self.surf = surf
-        # self.surf.width = charWidth
-        # self.surf.height = charHeight*(self.tailLength+1)
-        lastChar = self.textFont.render(chr(self.value), 1, white)
-        self.surf.blit(lastChar, (0, (self.tailLength)*charHeight))
+        # Create a new object for the character and tail to be on
+        targetChar = self.textFont.render(chr(self.value), 1, white)
+        # This is the character at the bottom that you're supposed to type
+        self.surf.blit(targetChar, (0, (self.tailLength)*charHeight))
         for i in range(len(self.tail)):
             uni = self.tailFont.render(self.tail[i], 1, green)
             self.surf.blit(uni, (0, ((i)*charHeight)))
